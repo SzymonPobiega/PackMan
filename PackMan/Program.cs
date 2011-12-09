c7d84e7612401085b56326f8a5d61061e5ee0690
Szymon Pobiega <szymon.pobiega@gmail.com>
2011-12-09 05:21:39 +0100

    Added libs

--- PackMan/Program.cs
+++ PackMan/Program.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
+using NDesk.Options;
 
 namespace PackMan
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            var printHelp = false;
+            var optionSet = new OptionSet()
+                .Add("h|?|help", "Print this help information", x =>
+                                                               {
+                                                                   printHelp = x != null;
+                                                               });
+
+            var result = optionSet.Parse(args);
+
+            if (printHelp)
+            {
+                Console.WriteLine("Usage: packman.exe [-h]");
+                Console.WriteLine("Available options:");
+                optionSet.WriteOptionDescriptions(Console.Out);
+            }
+
+            Console.ReadLine();
+
+            //var packageBytes = BuildPackage(args);
+
+            //var hash = ComputeHash(packageBytes);
+
+            //var signature = Sign(hash, args);
+
+            //WritePackage(args, packageBytes);
+            //WriteSignature(args, signature);
         }
     }
 }
